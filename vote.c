527440c04cd75996df2e9cee5bc49e48bc1ad269
Fro960 <106296793+Fro960@users.noreply.github.com>
2026-04-16 11:12:08 +0530

    Vote eligibility

---
+++ vote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+    {
+        printf("Eligible\n");
+    }
+
+    else
+    {
+        printf("Not Eligible\n");
+    }
+}
